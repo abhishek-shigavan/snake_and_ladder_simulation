070b9a2ceefc6040c767e6503b7a10adb86013f0
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-10 20:55:23 +0530

    Add: Playing condition added

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,16 +1,22 @@
 /**
- * SnakeAndLadder 
+ * SnakeAndLadder
  *
  * Program to roll dice and get result.
+ * Also checking the playing condition
+ * i.e No Play / Ladder / Snake
+ * 
  * 
  * @author    Abhishek Shigavan
  */
-package snakeandladdersimulation;
-
 public class SnakeAndLadder {
 
+	//constant... Playing Conditions of Game
+	public static final int NO_PLAY =0;
+	public static final int IS_LADDER=1;
+	public static final int IS_SNAKE=2;
+	
 	public static void main(String[] args) {
-		
+
 		//initial player position
 		int playerPosition=0;
 		
@@ -18,5 +24,18 @@ public class SnakeAndLadder {
 		
 		int diceNumber = (int)(Math.floor(Math.random() * 10) % 6)+1;
 		
+		int playingOption = (int)(Math.floor(Math.random() * 10) % 3);
+		
+		if(playingOption == IS_LADDER) {
+			playerPosition +=diceNumber;
+		}
+		else if(playingOption == IS_SNAKE){
+			playerPosition -=diceNumber;
+		}
+		else {
+			playerPosition=playerPosition; //NO_PLAY condition
+		}
+
 	}
 }
+
