a81306a88e18ad76d92087ab745908eaf2d3a085
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-10 20:41:15 +0530

    Add: Welcome message added

---
+++ SnakeAndLadder.java
@@ -0,0 +1,14 @@
+/**
+ * SnakeAndLadder 
+ *
+ * Program to roll dice and get result.
+ * 
+ * @author    Abhishek Shigavan
+ */
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welocme to Snake and Ladder Game...!!!");
+	}
+}
